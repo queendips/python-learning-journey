9cccd72c8fe472441c994f9c19e87e51aba6edb5
Dipsqueen <dipali@gmail.com>
2026-07-31 09:36:37 +0530

    adding day16

---
+++ Day-16 OOP (Class & Object)/01_class.py
@@ -0,0 +1,7 @@
+# Creating a simple class
+
+class Car:
+    pass
+# pass means:
+# "I'll write the code later."
+print("Class Car created successfully!")
---
+++ Day-16 OOP (Class & Object)/02_object.py
@@ -0,0 +1,12 @@
+# Creating objects from a class
+
+class Car:
+    pass
+
+car1 = Car()
+car2 = Car()
+car3 = Car()
+
+print(car1)
+print(car2)
+print(car3)
